b6cc82a309e4faa1ad94da070155fe53acb67887
Rostyslav-afk <110056857+Rostyslav-afk@users.noreply.github.com>
2025-03-30 11:23:19 +0200

    full

--- script.js
+++ script.js
@@ -17,23 +17,19 @@ console.log(friends.join(","));
 
 
 //#2 Працюємо з колекцією карток в trello. Метод splice() (можна використати інші методи)
-const cards = ['Карточка-1', 'Карточка-2', 'Карточка-3', 'Карточка-4', 'Карточка-5', 'Карточка-6']
-cards.splice(3, 1, 'банани', 'помідори')
+const cards = ['Карточка-1', 'Карточка-2', 'Карточка-3', 'Карточка-4', 'Карточка-5']
 console.log(`Карточки(2 завдання): ${cards}`);
 
 //#3 Видалити
-const cardToRemove = ['Карточка-3']
-cardToRemove.splice(0,1)
-console.log(`Карточки(3 завдання): ${cardToRemove}`);
+cards.splice(2, 1)
+console.log(`Карточки(3 завдання): ${cards}`);
 
 
 //#4 Додати
-const cardToInsert = ['Карточка-6']
-cardToInsert.splice(0, 0, "Карточка-5")
-console.log(`Карточки(4 завдання): ${cardToInsert}`);
+cards.splice(4  , 0, "Карточка-6")
+console.log(`Карточки(4 завдання): ${cards}`);
 
 
 //#5 Оновити
-const cardToUpdate = ['Карточка-4']
-cardToUpdate.splice(0, 1, "Карточка-1")
-console.log(`Карти(5 завдання): ${cardToUpdate}`);
+cards.splice(2, 1, "Оновлена карточка-4")
+console.log(`Карти(5 завдання): ${cards}`);
